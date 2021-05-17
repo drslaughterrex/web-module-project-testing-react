7accd081f5128661845d2902363c97712ed062e7
drslaughterrex <drslaughterrex@gmail.com>
2021-05-17 15:21:10 -0500

    MVP

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -1,17 +1,70 @@
+import React from "react";
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import Display from "./../Display";
+import mockFetchShow from "../../api/fetchShow";
+jest.mock("../../api/fetchShow");
 
+const testShow = {
+	//add in approprate test data structure here.
+	name: "Band of Brothers",
+	summary: "Maj. Dick Winters",
+	seasons: [
+		{
+			id: 0,
+			name: "Season 1",
+			episodes: [],
+		},
+		{
+			id: 1,
+			name: "Season 2",
+			episodes: [],
+		},
+	],
+};
 
+test("Renders Display with No Props", () => {
+	render(<Display />);
+});
 
+test("Test that when the fetch button is pressed, the show component will display", async () => {
+	render(<Display />);
 
+	mockFetchShow.mockResolvedValueOnce(testShow);
 
+	const button = screen.getByRole("button", /press to get show data/i);
+	userEvent.click(button);
 
+	const show = await screen.findByTestId("show-container");
 
+	expect(show).toBeInTheDocument();
+	expect(show).toHaveTextContent(/one piece/i);
+});
 
+test("Test that when the fetch button is pressed, the amount of select options rendered is equal to the amount of seasons in your test data", async () => {
+	render(<Display />);
 
+	mockFetchShow.mockResolvedValueOnce(testShow);
 
+	const button = screen.getByRole("button", /press to get show data/i);
+	userEvent.click(button);
 
+	const season = await screen.findAllByTestId("season-option");
 
+	expect(season).toHaveLength(2);
+});
 
+test("Test that when the fetch button is pressed, the amount of select options rendered is equal to the amount of seasons in your test data.", () => {
+	const mockDisplayFunc = jest.fn();
+	render(<Display displayFunc={mockDisplayFunc} />);
 
+	mockFetchShow.mockResolvedValueOnce(testShow);
+
+	const button = screen.getByRole("button", /press to get show data/i);
+	userEvent.click(button);
+
+	waitFor(() => expect(mockDisplayFunc).toHaveBeenCalledTimes(1));
+});
 
 ///Tasks:
 //1. Add in nessisary imports and values to establish the testing suite.
